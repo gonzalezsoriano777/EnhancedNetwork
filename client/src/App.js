74b6cb755bd4abd1d0941913d087b0bcc274e1dc
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-04-21 17:31:25 -0500

    Created a Protected Route PrivateRoute for the Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React, {
-  Fragment, useEffect
-} from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 //Redux
 import { Provider } from 'react-redux';
@@ -20,7 +20,6 @@ if (localStorage.token) {
 }
 
 const App = () => {
-
   useEffect(() => {
     store.dispatch(loadUser(), []);
   });
@@ -30,18 +29,19 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path="/" component={Landing} />
-          <section className="container">
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
             <Alert />
             <Switch>
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
       </Router>
     </Provider>
-  )
+  );
 };
 
 export default App;
